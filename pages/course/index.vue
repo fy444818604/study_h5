b9200e4f2049681cce145c7024285360195e4b54
fy <444818604@qq.com>
2020-11-23 17:56:41 +0800

    fu

--- pages/course/index.vue
+++ pages/course/index.vue
@@ -1,13 +1,36 @@
 <template>
 	<view class="content">
-		<ul class="course" @ontouch="showDiloag">
-			<li v-for="item in menu">{{item}}</li>
+		<ul class="course">
+			<li v-for="(item,index) in menu" @tap="showDiloag(index)">{{item}}</li>
 		</ul>
 		<ul class="filter-wrap">
-			<li>
+			<li v-show="searchNum == 1">
 				<ul class="subject-wrap">
-					<li v-for="item in courseList">{{item.subjectName}}</li>
+					<li v-for="item in courseList" :class="item.active?'active':''">{{item.subjectName}}</li>
 				</ul>
+			</li>
+			<li v-show="searchNum == 2">
+				<ul class="subject-wrap">
+					<li v-for="item in orderShow" :class="item.active?'active':''">{{item.title}}</li>
+				</ul>
+			</li>
+			<li v-show="searchNum == 3">
+				<div class="condition-filter">
+					<div class="condition-filter-title">课程类型</div>
+					<ul>
+						<li v-for="item in courseType" :class="item.active?'active':''">{{item.title}}</li>
+					</ul>
+				</div>
+				<div class="condition-filter">
+					<div class="condition-filter-title">学员分类</div>
+					<ul>
+						<li v-for="item in memberTypeList" :class="item.active?'active':''">{{item.title}}</li>
+					</ul>
+				</div>
+				<div class="condition-bottom">
+					<button type="default" class="left-btn">重置</button>
+					<button type="default" class="right-btn">确定</button>
+				</div>
 			</li>
 		</ul>
 		<ul class="course-ul">
@@ -24,7 +47,7 @@
 				</navigator>
 			</li>
 		</ul>
-		<uni-popup ref="popup" type="bottom">底部弹出 Popup</uni-popup>
+		<uni-popup ref="popup" type="bottom" @change="popChange">底部弹出 Popup</uni-popup>
 	</view>
 </template>
 
@@ -32,69 +55,127 @@
 	export default {
 		data() {
 			return {
-				menu: ['课程专业','排序方式','条件筛选'],
-				courseList:[{
+				menu: ['课程专业', '排序方式', '条件筛选'],
+				searchNum: 0,
+				courseList: [{
 					subjectId: 0,
-					subjectName:'全部'
-				},{
+					subjectName: '全部',
+					active: true
+				}, {
 					subjectId: 1,
-					subjectName:'小学'
-				},{
+					subjectName: '小学',
+					active: false
+				}, {
 					subjectId: 2,
-					subjectName:'初中'
-				},{
+					subjectName: '初中',
+					active: false
+				}, {
 					subjectId: 3,
-					subjectName:'高中'
-				},{
+					subjectName: '高中',
+					active: false
+				}, {
 					subjectId: 4,
-					subjectName:'职高'
-				},{
+					subjectName: '职高',
+					active: false
+				}, {
 					subjectId: 5,
-					subjectName:'学前'
-				},{
+					subjectName: '学前',
+					active: false
+				}, {
 					subjectId: 4,
-					subjectName:'职高'
-				},{
+					subjectName: '职高',
+					active: false
+				}, {
 					subjectId: 5,
-					subjectName:'学前'
+					subjectName: '学前',
+					active: false
 				}],
-				sortList: [],
+				orderShow: [{
+						title: "综合排序",
+						key: "BYGONE",
+						active: true
+					},
+					{
+						title: "最新",
+						key: "NEW",
+						active: false
+					},
+					{
+						title: "最热",
+						key: "FOLLOW",
+						active: false
+					},
+					{
+						title: "学分从低到高",
+						key: "ASCENDING",
+						active: false
+					},
+					{
+						title: "学分从高到低",
+						key: "DESCENDING",
+						active: false
+					}
+				],
+				courseType: [{
+					title: '录播课程',
+					key: 'PACKAGE',
+					active: true
+				}, {
+					title: '课程集',
+					key: 'true',
+					active: false
+				}],
+				memberTypeList: [{
+					title: '全部',
+					active: true
+				}, {
+					title: '一年级',
+					active: false
+				}, {
+					title: '二年级',
+					active: false
+				}, {
+					title: '三年级',
+					active: false
+				}],
 				allCouList: [{
-					courseName:'8月22号小周末线上学习'
-				},{
-					courseName:'8月22号小周末线上学习'
-				},{
-					courseName:'8月22号小周末线上学习'
-				},{
-					courseName:'8月22号小周末线上学习'
-				},{
-					courseName:'8月22号小周末线上学习'
-				},]
+					courseName: '8月22号小周末线上学习'
+				}, {
+					courseName: '8月22号小周末线上学习'
+				}, {
+					courseName: '8月22号小周末线上学习'
+				}, {
+					courseName: '8月22号小周末线上学习'
+				}, {
+					courseName: '8月22号小周末线上学习'
+				}, ]
 			}
 		},
 		onLoad() {
-			
-		},
-		mounted() {
-			this.$refs.popup.open()
+
 		},
 		methods: {
-			showDiloag() {
-				console.log(123)
+			showDiloag(index) {
+				this.searchNum = index + 1
 				this.$refs.popup.open()
+			},
+			popChange(e) {
+				if (!e.show) {
+					this.searchNum = 0
+				}
 			}
 		}
 	}
 </script>
 
-<style>
-	.course{
+<style lang="scss">
+	.course {
 		display: flex;
 		position: relative;
 		z-index: 998;
 	}
-	
-	.course>li{
+
+	.course>li {
 		width: 33.33%;
 		flex: 1;
 		text-align: center;
@@ -106,46 +187,46 @@
 		z-index: 2;
 		background: #fff;
 	}
-	
+
 	.course>li:after {
-    content: '';
-    display: inline-block;
-    vertical-align: 4rpx;
-    width: 0;
-    height: 0;
-    border-left: 6rpx solid transparent;
-    border-right: 6rpx solid transparent;
-    border-top: 6rpx solid #bbb;
-    margin-left: 4rpx;
+		content: '';
+		display: inline-block;
+		vertical-align: 4rpx;
+		width: 0;
+		height: 0;
+		border-left: 12rpx solid transparent;
+		border-right: 12rpx solid transparent;
+		border-top: 12rpx solid #bbb;
+		margin-left: 8rpx;
 	}
-	
-	.course-item-wrap{
+
+	.course-item-wrap {
 		display: flex;
 	}
-	
-	.course-ul>li{
+
+	.course-ul>li {
 		border-bottom: 1px solid #E9E9E9;
-		padding: 10rpx 0;
+		padding: 20rpx 0;
 		position: relative;
 	}
-	
-	.course-img{
+
+	.course-img {
 		width: 253rpx;
 		height: 141rpx;
 		margin: 0 20rpx;
 		overflow: hidden;
 	}
-	
-	.course-img>image{
+
+	.course-img>image {
 		width: 100%;
 		height: 100%;
 	}
-	
-	.course-title{
+
+	.course-title {
 		font-size: 32rpx;
 	}
-	
-	.filter-wrap{
+
+	.filter-wrap {
 		position: absolute;
 		display: flex;
 		background: #eee;
@@ -154,15 +235,80 @@
 		max-height: 550rpx;
 		z-index: 998;
 	}
-	.filter-wrap>li{
+
+	.filter-wrap>li {
 		width: 100%;
 	}
-	
+
 	.subject-wrap>li {
 		height: 90rpx;
 		display: flex;
 		justify-content: center;
 		align-items: center;
 	}
-	
+
+	.subject-wrap>li.active {
+		background: #fff;
+	}
+
+	.condition-filter-title {
+		height: 80rpx;
+		line-height: 80rpx;
+		text-align: left;
+		margin-left: 20rpx;
+	}
+
+	.condition-filter {
+		border-bottom: 1px dashed #ddd;
+		margin-bottom: 20rpx;
+	}
+
+	.condition-filter>ul {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: flex-start;
+	}
+
+	.condition-filter>ul>li {
+		color: #666;
+		margin: 0 0 10rpx 20rpx;
+		height: 50rpx;
+		line-height: 47rpx;
+		padding: 0 10px;
+		background: #fff;
+		border-radius: 4px;
+		border: 1px solid transparent;
+		text-align: center;
+	}
+
+	.condition-filter>ul>li.active {
+		background: #ffd7ca;
+		border: 1px solid #ff662f;
+		color: #ff662f;
+	}
+
+	.condition-bottom {
+		display: flex;
+		justify-content: flex-end;
+	}
+
+	.condition-btn {
+		margin: 5rpx 20rpx 20rpx 0;
+		padding: 0 20px;
+		font-size: 12px;
+	}
+
+	.left-btn {
+		@extend .condition-btn;
+		background: #ffd7ca;
+		border: 1px solid #ff662f;
+		color: #ff662f;
+	}
+
+	.right-btn {
+		@extend .condition-btn;
+		border: 1px solid #ff662f;
+		color: #fff;
+		background: #ff662f;
+	}
 </style>
